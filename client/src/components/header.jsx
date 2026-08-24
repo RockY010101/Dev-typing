943e4a248ea087da58dac342ba070e0f3cfceae1
Abhixarvar <abhisarvarshney141204@gmail.com>
2026-08-24 19:01:26 +0530

    Hide Home and Back buttons on landing page

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -5,6 +5,7 @@ import MusicPlayer from "./MusicPlayer";
 function Header() {
     const location = useLocation();
     const navigate = useNavigate();
+    const isHomePage = location.pathname === '/';
     const isPlayingPage = location.pathname.toLowerCase() === '/typing';
 
     const handleBack = () => {
@@ -17,25 +18,27 @@ function Header() {
 
     return (
         <header className="header-container">
-            {/* Top Left: Music Player + Home & Back buttons */}
+            {/* Top Left: Music Player + Home & Back buttons (hidden on Home page) */}
             <div className="header-left">
                 <MusicPlayer />
-                <div className="header-nav-group">
-                    <button 
-                        className="header-nav-btn" 
-                        onClick={() => navigate('/')}
-                        title="Go to Home"
-                    >
-                        Home
-                    </button>
-                    <button 
-                        className="header-nav-btn" 
-                        onClick={handleBack}
-                        title="Go Back"
-                    >
-                        Back
-                    </button>
-                </div>
+                {!isHomePage && (
+                    <div className="header-nav-group">
+                        <button 
+                            className="header-nav-btn" 
+                            onClick={() => navigate('/')}
+                            title="Go to Home"
+                        >
+                            Home
+                        </button>
+                        <button 
+                            className="header-nav-btn" 
+                            onClick={handleBack}
+                            title="Go Back"
+                        >
+                            Back
+                        </button>
+                    </div>
+                )}
             </div>
 
             {/* Top Right: Auth & Navigation links (Profile, Leaderboard) - Hidden when playing */}
